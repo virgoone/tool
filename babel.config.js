811f9fe52eb7859799a7f10b9221c17bb72acbaf
guo <koya@shuaibing.guo>
2020-04-19 16:57:49 +0800

    feat(init): initial commit

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+module.exports = api => {
+  api.cache(true)
+
+  return {
+    plugins: [
+      [
+        'import',
+        {
+          libraryName: 'antd',
+          libraryDirectory: 'lib',
+          style: 'css',
+        },
+      ],
+    ],
+  }
+}
---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import '@/style/index.less'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+window.__APP_LOADED__ = true
+
+const init = () => {
+  const App = require('./App').default
+
+  ReactDOM.render(<App />, document.getElementById('root'))
+}
+
+if (__DEV__) {
+  module.hot.accept('./App', init)
+}
+
+init()
